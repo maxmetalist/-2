26d15804e544689ed917efe9a62917a9cc8fb1f8
Max <z.max83@mail.ru>
2025-03-04 11:59:26 +0300

    изменения в файле processing.py, тест функции сортировки

--- src/processing.py
+++ src/processing.py
@@ -1,23 +1,19 @@
-def filter_by_state(id_list: list, state: str = "EXECUTED") -> list:
-    """Функция сортировки по ключу состояния id клиента"""
-    executed_list = []
-    for id in id_list:
-        if id["state"] == "EXECUTED":
-            executed_list.append(id)
+#def filter_by_state(id_list: list, state: str = "EXECUTED") -> list:
+    #"""Функция сортировки по ключу состояния id клиента"""
+    #executed_list = []
+    #for id in id_list:
+        #if id["state"] == "EXECUTED":
+            #executed_list.append(id)
 
-    return executed_list
+    #return executed_list
 
 
-print(
-    filter_by_state(
-        [
+#print(
+    #filter_by_state([
             {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
             {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
             {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-        ]
-    )
-)
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},]))
 
 
 def sort_by_date(id_list: list, reverse: bool = True) -> list:
